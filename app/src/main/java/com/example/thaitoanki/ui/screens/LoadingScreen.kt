319a7ec66f620ccf00035fae828cc68da263ddc1
kzhangdz <kevin.zhang@detroitmi.gov>
2024-12-13 00:38:57 -0500

    comments for future work

--- app/src/main/java/com/example/thaitoanki/ui/screens/LoadingScreen.kt
+++ app/src/main/java/com/example/thaitoanki/ui/screens/LoadingScreen.kt
@@ -2,6 +2,9 @@ package com.example.thaitoanki.ui.screens
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -11,9 +14,16 @@ import com.example.thaitoanki.R
 
 @Composable
 fun LoadingScreen(modifier: Modifier = Modifier) {
-    Image(
-        modifier = modifier.size(200.dp),
-        painter = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource(R.string.loading)
+
+    CircularProgressIndicator(
+        modifier = Modifier.width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant,
     )
+
+//    Image(
+//        modifier = modifier.size(200.dp),
+//        painter = painterResource(R.drawable.loading_img),
+//        contentDescription = stringResource(R.string.loading)
+//    )
 }
