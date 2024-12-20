916fdf72a9f1a9b88a974c28cc2d139055470411
kzhangdz <kevin.zhang@detroitmi.gov>
2024-12-19 22:58:55 -0500

    current sentence indices

--- app/src/main/java/com/example/thaitoanki/ui/screens/ThaiLanguageUiState.kt
+++ app/src/main/java/com/example/thaitoanki/ui/screens/ThaiLanguageUiState.kt
@@ -6,5 +6,6 @@ data class ThaiLanguageUiState(
     val currentWord: String = "",
     val currentDefinitionIndex: Int = 0,
     val currentDefinitions: List<Definition> = listOf(),
+    val currentSentenceIndices: List<Int?> = listOf() // represents the selected sentence for a definition i.e. [0, null, 5, 2, null, 0]
     //val definition: String = ""
 )
