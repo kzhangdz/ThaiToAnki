08f7a4039ccb70d8d274f654b7298de85516f4f6
kzhangdz <kevin.zhang@detroitmi.gov>
2025-01-05 21:18:20 -0500

    adding build for flashcard page

--- app/src/main/java/com/example/thaitoanki/ui/screens/components/FlashcardFront.kt
+++ app/src/main/java/com/example/thaitoanki/ui/screens/components/FlashcardFront.kt
@@ -1,7 +1,12 @@
 package com.example.thaitoanki.ui.screens.components
 
+import android.content.Context
+import android.text.Layout
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.IdRes
@@ -33,56 +38,122 @@ fun FlashcardFront(
         },
         modifier = modifier,
         update = { view ->
-            // set the header text with word info
-            // word
-            val titleTextView = view.findViewById<TextView>(R.id.word)
-            titleTextView.setText(currentFlashcard.baseWord)
-
-            // pronunciation
-            val pronunciationTextView = view.findViewById<TextView>(R.id.pronunciation)
-            pronunciationTextView.setText(R.string.pronunciation_label)
-
-            // onClickListener
-
-            // romanization
-
-            
-            // information sections
-            // partOfSpeech
-            val partOfSpeechTextView = view.findViewById<TextView>(R.id.partOfSpeech)
-            if (currentFlashcard.partOfSpeech.isNotEmpty()){
-                partOfSpeechTextView.setText(currentFlashcard.partOfSpeech)
-            }
-            else{
-                val definitionSectionView: View = view.findViewById<LinearLayout>(R.id.definition_container)
-                (definitionSectionView.getParent() as ViewGroup).removeView(definitionSectionView)
-            }
-
-            // definition
-            val definitionTextView = view.findViewById<TextView>(R.id.definition)
-            if (currentFlashcard.definition.isNotEmpty()){
-                definitionTextView.setText(currentFlashcard.definition)
-            }
-            else{
-                val definitionSectionView: View = view.findViewById<LinearLayout>(R.id.definition_container)
-                (definitionSectionView.getParent() as ViewGroup).removeView(definitionSectionView)
-            }
-
-            // classifier
-
-            // components
-
-            // synonyms
-
-            // related words
-
-            // examples
-
-            // sentences
-
-            // reference
+            updateFlashcardFrontView(view, currentFlashcard)
         }
     )
+}
+
+// TODO: move these functions out if needed in the service
+fun updateFlashcardFrontView(view: View, currentFlashcard: Definition){
+    // variables used for adding views to sections
+    val context = view.context.applicationContext
+    val layoutInflater: LayoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    // set the header text with word info
+    // word
+    val titleTextView = view.findViewById<TextView>(R.id.word)
+    titleTextView.setText(currentFlashcard.baseWord)
+
+    // pronunciation
+    val pronunciationTextView = view.findViewById<TextView>(R.id.pronunciation)
+    pronunciationTextView.setText(R.string.pronunciation_label)
+
+    // onClickListener
+
+    // romanization
+
+
+    // information sections
+    // partOfSpeech
+    val partOfSpeechTextView = view.findViewById<TextView>(R.id.partOfSpeech)
+    if (currentFlashcard.partOfSpeech.isNotEmpty()){
+        partOfSpeechTextView.setText(currentFlashcard.partOfSpeech)
+    }
+    else{
+        val definitionSectionView: View = view.findViewById<LinearLayout>(R.id.definition_container)
+        (definitionSectionView.getParent() as ViewGroup).removeView(definitionSectionView)
+    }
+
+    // definition
+    val definitionTextView = view.findViewById<TextView>(R.id.definition)
+    if (currentFlashcard.definition.isNotEmpty()){
+        definitionTextView.setText(currentFlashcard.definition)
+    }
+    else{
+        val definitionSectionView: View = view.findViewById<LinearLayout>(R.id.definition_container)
+        (definitionSectionView.getParent() as ViewGroup).removeView(definitionSectionView)
+    }
+
+    // classifier
+//    val classifierSectionViewId = R.id.classifier_container
+//    buildSection(view,
+//        sectionInfo = currentFlashcard.classifiers,
+//        containerId = classifierSectionViewId,
+//        build = {
+//
+//        })
+
+    // components
+    val componentsSectionViewId = R.id.components_container
+    buildSection(view,
+        sectionInfo = currentFlashcard.components,
+        containerId = componentsSectionViewId,
+        build = {
+            val parent = view.findViewById<LinearLayout>(R.id.components_content)
+
+            for (i in 0..<currentFlashcard.components.size){
+                //val newView = View()
+                //parent.addView(R.layout.fragment_pill)
+                //val pill = layoutInflater.inflate(R.layout.fragment_pill, parent) as Button
+
+                // TODO: might need an arrayAdapter for this to work
+                // Might also need to switch to ListView
+                val pill = View.inflate(context, R.layout.fragment_pill, parent) as ViewGroup
+
+                Log.d("test", pill.getChildAt(0).toString())
+                //https://stackoverflow.com/questions/8395168/android-get-children-inside-a-view
+
+//                val pillButton = view.findViewById<Button>(R.id.pill)
+//                pillButton.text = "test"
+//
+//                view.findView
+
+                // set the id
+                //pill.setTag(i)
+                //pill.id = View.generateViewId()
+
+                // adjust the text inside
+                //pill.text = "text"//(currentFlashcard.components[i].baseWord)
+            }
+
+//            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            ViewGroup parent = (ViewGroup)findViewById(R.id.where_you_want_to_insert);
+//            inflater.inflate(R.layout.the_child_view, parent);
+        })
+
+    // synonyms
+
+    // related words
+
+    // examples
+    val exampleSectionViewId = R.id.examples_container
+    buildSection(view,
+        sectionInfo = currentFlashcard.examples,
+        containerId = exampleSectionViewId,
+        build = {
+            val parent = view.findViewById<LinearLayout>(R.id.examples_section)
+
+
+            val newView = layoutInflater.inflate(R.layout.fragment_pill, parent)
+
+//            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//            ViewGroup parent = (ViewGroup)findViewById(R.id.where_you_want_to_insert);
+//            inflater.inflate(R.layout.the_child_view, parent);
+        })
+
+    // sentences
+
+    // reference
 }
 
 // display or hide sections of the flashcard
@@ -107,7 +178,7 @@ fun FlashcardFrontPreview() {
             modifier = Modifier
                 .padding(dimensionResource(R.dimen.padding_medium))
                 .verticalScroll(rememberScrollState()),
-            currentDefinitionIndex = 0,
+            currentDefinitionIndex = 1,
         )
     }
 }
