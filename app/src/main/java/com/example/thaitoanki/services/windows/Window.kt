0270f559ebc0431b9b602478cd9a80f058eb7cc0
kzhangdz <kevin.zhang@detroitmi.gov>
2025-01-09 19:44:19 -0500

    notes

--- app/src/main/java/com/example/thaitoanki/services/windows/Window.kt
+++ app/src/main/java/com/example/thaitoanki/services/windows/Window.kt
@@ -129,6 +129,23 @@ class Window(context: Context,
 
                     // closing and opening the window affected the search insertion somehow
                     // rather than closing the window, should I move it offscreen and show another one?
+
+                    // have the definition window open off screen?
+                    // then after a word is saved, use a broadcast receiver notify the definition window and retrieve definitions?
+                    // try to do it by collecting from the flow first
+
+                    // I think I can open multiple windows. max is 300
+                    // can probably put it directly over the searchWindow's
+
+                    // next step for now is to make an inheritable window
+
+                    // An overlay is just a layout, without an underlying activity
+
+
+
+                    // Can I actually just do an overlay activity instead?
+                    // This link uses an intent to start a second activity that's based in a window manager
+                    // https://stackoverflow.com/questions/18843868/android-overlay-on-window-over-activities-of-a-task
                 }
             }
         }
