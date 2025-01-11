e4973bc775946ec76c0667ddc5116b29c01a2283
kzhangdz <kevin.zhang@detroitmi.gov>
2025-01-11 01:09:01 -0500

    onclick for pill

--- app/src/main/java/com/example/thaitoanki/ui/screens/adapters/PillListAdapter.kt
+++ app/src/main/java/com/example/thaitoanki/ui/screens/adapters/PillListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.thaitoanki.R
 import com.example.thaitoanki.data.network.Definition
@@ -18,12 +19,13 @@ class PillListAdapter(private val definitions: List<Definition>) :
      */
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         // need to declare all the elements in the card
-        val pillView: TextView
+        val pillView: CardView
+        val pillTextView: TextView
 
         init {
             // Define click listener for the ViewHolder's View
-            pillView = view.findViewById<TextView>(R.id.pill)
-
+            pillView = view.findViewById<CardView>(R.id.pill)
+            pillTextView = view.findViewById<TextView>(R.id.pill_text)
         }
     }
 
@@ -41,7 +43,22 @@ class PillListAdapter(private val definitions: List<Definition>) :
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.pillView.text = definitions[position].baseWord
+        viewHolder.pillTextView.text = definitions[position].baseWord
+
+        viewHolder.pillView.setOnClickListener(){
+            val isNotExpanded: Boolean = viewHolder.pillTextView.text == definitions[position].baseWord
+
+            // modify the pill text to display a paragraph
+            if (isNotExpanded){
+                // expand
+                viewHolder.pillTextView.text = definitions[position].baseWord + "\n" + definitions[position].definition
+            }
+            else{
+                viewHolder.pillTextView.text = definitions[position].baseWord
+            }
+
+            // need way to unclick
+        }
     }
 
     // Return the size of your dataset (invoked by the layout manager)
